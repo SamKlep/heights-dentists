2fde45a4d523eafce0b7356d943aebad6fa96602
SamKlep <shamdemann23@gmail.com>
2021-01-12 14:57:42 -0600

    Navbar & Carousel styling/content.

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,9 +1,42 @@
-import { Jumbotron } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap'
 
 const Main = () => {
   return (
-    <div className='container'>
-      <Jumbotron className='jumbo-image' />
+    <div className='container mb-5'>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/img/dentist-office1.jpg'
+            alt='First slide'
+          />
+          <Carousel.Caption>
+            <h3>Patient & Professinal</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/img/dentist-office2.jpg'
+            alt='Third slide'
+          />
+
+          <Carousel.Caption>
+            <h3>Preventative Care</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/img/dentist-exam.jpeg'
+            alt='Third slide'
+          />
+
+          <Carousel.Caption>
+            <h3>Knowledgeable & Caring</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   )
 }
--- src/components/ui/Navigator.js
+++ src/components/ui/Navigator.js
@@ -12,22 +12,22 @@ const Navigator = () => {
         <Navbar.Collapse
           className='justify-content-center'
           id='basic-navbar-nav'>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link href='/'>HOME</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link eventKey='link-1'>ABOUT</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link eventKey='link-2'>COSMETIC</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link href='/home'>RESTORATIVE</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link eventKey='link-1'>PREVENTATIVE</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className='navigation-links'>
             <Nav.Link eventKey='link-2'>CONTACT</Nav.Link>
           </Nav.Item>
         </Navbar.Collapse>
